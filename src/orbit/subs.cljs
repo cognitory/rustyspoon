f5efd0d9a25950d3faae75c7c3f7624697bdfc81
Rafal Dittwald <rafal.dittwald@gmail.com>
2017-03-29 20:52:44 -0400

    Use reg-sub instead of register-sub

--- src/orbit/subs.cljs
+++ src/orbit/subs.cljs
@@ -2,38 +2,37 @@
   (:require
     [clojure.string :as string]
     [fipp.clojure :as fipp]
-    [reagent.ratom :include-macros true :refer-macros [reaction]]
-    [re-frame.core :as rf]))
+    [re-frame.core :refer [reg-sub]]))
 
-(rf/register-sub
+(reg-sub
   :get-steps
-  (fn [app-state _]
-    (reaction (get-in @app-state [:orbit :history]))))
+  (fn [db _]
+    (get-in db [:orbit :history])))
 
-(rf/register-sub
+(reg-sub
   :get-current-resources
-  (fn [app-state _]
-     (reaction (get-in @app-state [:orbit :history (:step @app-state) :resources]))))
+  (fn [db _]
+     (get-in db [:orbit :history (:step db) :resources])))
 
-(rf/register-sub
+(reg-sub
   :get-content
-  (fn [app-state _]
-    (reaction (get-in @app-state [:content]))))
+  (fn [db _]
+    (get-in db [:content])))
 
-(rf/register-sub
+(reg-sub
   :get-code-for-step
-  (fn [app-state [_ step-id index-start index-end]]
+  (fn [db [_ step-id index-start index-end]]
     (let [index-end (if (js/isNaN index-end) index-start index-end)
-          code (-> (get-in @app-state [:orbit :history])
+          code (-> (get-in db [:orbit :history])
                    (->> (filter (fn [s] (= step-id (s :step)))))
                    first
                    (get :step-actions)
                    (subvec index-start (inc index-end))
                    (->> (map #(with-out-str (fipp/pprint %1 {:width 50}))))
                    (->> (string/join "\n")))]
-    (reaction code))))
+      code)))
 
-(rf/register-sub
+(reg-sub
   :get-current-step
-  (fn [app-state _]
-    (reaction (:step @app-state))))
+  (fn [db _]
+    (:step db)))
