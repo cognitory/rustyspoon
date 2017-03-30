506eb4eaadac2488d8a48d790d24c949bcabf727
Rafal Dittwald <rafal.dittwald@gmail.com>
2017-03-29 21:10:07 -0400

    Move eval-code into its own namespace and use it as a reg-fx

---
+++ src/orbit/eval.cljs
@@ -0,0 +1,22 @@
+(ns orbit.eval
+  (:require
+    [clojure.string :as string]
+    [cljs.js :refer [empty-state eval-str js-eval]]))
+
+(defn eval-code [code]
+  (eval-str (empty-state)
+            (string/join "\n" code)
+            'dummy-symbol
+            {:ns 'cljs.user
+             :static-fns true
+             :def-emits-var false
+             :eval js-eval
+             ; NOTE: load does nothing; libs must be reqd by this ns
+             :load (fn [name cb] (cb {:lang :clj :source "."}))
+             :context :statement}
+            (fn [{:keys [error value] :as x}]
+              (if error
+                (do
+                  (def *er x)
+                  (js/console.log (str error)))))))
+
--- src/orbit/events.cljs
+++ src/orbit/events.cljs
@@ -1,32 +1,14 @@
 (ns orbit.events
   (:require
-    [clojure.string :as string]
-    [cljs.js :refer [empty-state eval-str js-eval]]
-    [re-frame.core :refer [reg-event-fx]]))
+    [re-frame.core :refer [reg-event-fx reg-fx]]
+    [orbit.eval :refer [eval-code]]))
 
-(defn- eval-code [code]
-  (eval-str (empty-state)
-            (string/join "\n" code)
-            'dummy-symbol
-            {:ns 'cljs.user
-             :static-fns true
-             :def-emits-var false
-             :eval js-eval
-             ; NOTE: load does nothing; libs must be reqd by this ns
-             :load (fn [name cb] (cb {:lang :clj :source "."}))
-             :context :statement}
-            (fn [{:keys [error value] :as x}]
-              (if error
-                (do
-                  (def *er x)
-                  (js/console.log (str error)))))))
-
+(reg-fx :eval-code eval-code)
 
 (reg-event-fx
   :-eval-current-code
   (fn [{db :db} [_ step]]
-    (eval-code (get-in db [:orbit :history step :resources "core.cljs"]))
-    {}))
+    {:eval-code (get-in db [:orbit :history step :resources "core.cljs"])}))
 
 (reg-event-fx 
   :init! 
